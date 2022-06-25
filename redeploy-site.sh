8a56987df651d5c7eb6d3aa7a48590aca36b984c
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-06-25 17:23:50 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,1 +1,10 @@
-#!/bin/bash                                                                                                                                                                                                     # local repo update                                                                                     cd /root/project-blue-pandas/                                                                           git fetch && git reset origin/main --hard                                                               source python3-virtualenv/bin/activate                                                                  pip install -r requirements.txt                                                                                                                                                                                 # restart portfolio service                                                                             systemctl restart myportfolio 
+#!/bin/bash     
+
+# local repo update                                                                                     
+cd /root/project-blue-pandas/                                                                           
+git fetch && git reset origin/main --hard                                                               
+source python3-virtualenv/bin/activate                                                                  
+pip install -r requirements.txt                   
+
+# restart portfolio service                                                                             
+systemctl restart myportfolio 
