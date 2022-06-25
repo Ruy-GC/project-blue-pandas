7bcf646f549033cad3c790e3a8c1127fea0bab01
root <ruy.guzman2002@gmail.com>
2022-06-25 22:21:08 +0000

    update site deploy to service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-# kill tmux sessions
-pkill -f tmux
-
-# local repo update
-cd /root/project-blue-pandas/
-git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
-# new tmux session for deploy
-tmux new -d -s portfolio_deploy 'flask run --host=0.0.0.0'
-
+#!/bin/bash                                                                                                                                                                                                     # local repo update                                                                                     cd /root/project-blue-pandas/                                                                           git fetch && git reset origin/main --hard                                                               source python3-virtualenv/bin/activate                                                                  pip install -r requirements.txt                                                                                                                                                                                 # restart portfolio service                                                                             systemctl restart myportfolio 
